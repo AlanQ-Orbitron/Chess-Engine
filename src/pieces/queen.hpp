08356d9af99e155b830fccf961a4c12eaac927b5
AlanQ-Orbitron <alanvquin@gmail.com>
2026-01-14 08:48:53 -0800

    Update queen.hpp

--- src/pieces/queen.hpp
+++ src/pieces/queen.hpp
@@ -3,11 +3,9 @@
 #include "rook.hpp"
 
 struct Queen : virtual Bishop, virtual Rook {
-    Queen() {piece_type = Pieces::Queen;}
-    void generate_movement_moves(int square_index, GameState &board) const override {
-        Bishop::generate_movement_moves(square_index, board);
-        uint64_t bishop_movement_bitboard = shape_group.movement_bitboard;
-        Rook::generate_movement_moves(square_index, board);
-        shape_group.movement_bitboard |= bishop_movement_bitboard;
-    }
+	Queen() { piece_type = Pieces::Queen; }
+
+	uint64_t generate_movement_moves(int square_index, GameState &board) const override {
+		return Bishop::generate_movement_moves(square_index, board) | Rook::generate_movement_moves(square_index, board);
+	}
 };
