bf5aa67760846fa9c68898de92f69105beeef6cd
AlanQ-Orbitron <alanvquin@gmail.com>
2026-01-14 08:45:16 -0800

    Create includer.hpp

---
+++ src/pieces/includer.hpp
@@ -0,0 +1,11 @@
+#pragma once
+// IWYU pragma: begin_exports
+#include "bishop.hpp"
+#include "duck.hpp"
+#include "king.hpp"
+#include "knight.hpp"
+#include "pawn.hpp"
+#include "piece.hpp"
+#include "queen.hpp"
+#include "rook.hpp"
+// IWYU pragma: end_exports
